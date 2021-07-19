29fc9e532bee425767081ce23c1f52a4a096823a
adam tolley <adamt@hzdg.com>
2021-07-19 19:47:53 -0400

    fix theme query function to use string dimensions(2)

--- web/themes/theme.ts
+++ web/themes/theme.ts
@@ -22,9 +22,9 @@ export type ThemeSpaceScale = number[] &
   };
 
 // combined array scale with named keys
-export type ThemeBreakpointScale = number[] &
+export type ThemeBreakpointScale = string[] &
   {
-    [index in ThemeBreakpointName]: number;
+    [index in ThemeBreakpointName]: string;
   };
 
 export type Theme = {
@@ -67,7 +67,7 @@ space.x8 = space[8];
 space.x10 = space[9];
 space.x12 = space[10];
 
-const breakpoints = [768, 1024, 1440] as ThemeBreakpointScale;
+const breakpoints = ['768px', '1024px', '1440px'] as ThemeBreakpointScale;
 breakpoints.tablet = breakpoints[0];
 breakpoints.desktop = breakpoints[1];
 breakpoints.max = breakpoints[2];
