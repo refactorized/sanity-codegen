ff39d3c923fd7d3e28fffc6f90ca64cfa23b463d
Joe Panuncialman <joe@panuncialman.com>
2021-07-22 15:44:05 -0400

    TextAndImageBlock Component (#10)
    * Testing
    
    * Text and Image block component implementation (in progress)
    
    * Button component
    
    * Button fixes
    
    * Added stories for arrow variants
    
    * Added arrow
    
    * Finished styling
    
    * Updated directory to components folder
    
    * Configured storybook config to allow for mock images
    
    * Fixed styling for images
    
    * arrowColor added
    
    * Fixed tablet + desktop styling
    
    * In progress
    
    * theme file fix
    
    * Fixed styling
    
    Co-authored-by: adam tolley <adamt@hzdg.com>

---
+++ web/components/TextAndImageBlock/TextAndImageBlock.stories.tsx
@@ -0,0 +1,32 @@
+import React, {ComponentProps} from 'react';
+
+import {Story} from '@storybook/react';
+
+import {TextAndImageBlock} from './index';
+import {ThemeProvider} from 'styled-components';
+import theme from '../../themes';
+
+export default {
+  title: 'Components/TextAndImageBlock',
+  component: TextAndImageBlock,
+};
+
+const Template: Story<ComponentProps<typeof TextAndImageBlock>> = (args) => (
+  <ThemeProvider theme={theme}>
+    <TextAndImageBlock {...args} />
+  </ThemeProvider>
+);
+
+export const DefaultSetup = Template.bind({});
+DefaultSetup.args = {
+  header: 'Fresh Insights and a Century of Expertise',
+  imgUrls: {
+    desktop: '/text-and-img-block.jpg',
+    mobile: '/text-and-img-block.jpg',
+  },
+  subheader: 'Education & Research',
+  caption:
+    'The Erikson Institute for Education and Research of the Austen Riggs Center is about the advancement of knowledge. We host and attend conferences, workshops, lecture series, and events. The Institute also offers national and international professional education.',
+  btnText: 'Learn More',
+  btnUrl: '#',
+};
---
+++ web/components/TextAndImageBlock/index.tsx
@@ -0,0 +1,128 @@
+import styled from 'styled-components';
+import {
+  color,
+  space,
+  typography,
+  flexbox,
+  layout,
+  grid,
+  size,
+  letterSpacing,
+} from 'styled-system';
+import Image from 'next/image';
+import {Button} from '../../components/Button/index';
+export interface TextAndImageBlockProps {
+  header: string;
+  imgUrls?: {
+    desktop: string;
+    mobile: string;
+  };
+  subheader: string;
+  caption: string;
+  btnText?: string;
+  btnUrl?: string;
+}
+
+/*** STYLING ***/
+const Box = styled.div`
+  ${color}
+  ${typography}
+  ${flexbox}
+  ${space}
+  ${layout}
+  ${grid}
+  ${size}
+`;
+
+const Header = styled.h1`
+  ${color}
+  ${space}
+  ${typography}
+`;
+
+const Subheader = styled.h2`
+  ${color}
+  ${space}
+  ${typography}
+  text-transform: uppercase;
+`;
+
+const Text = styled.p`
+  ${color}
+  ${space}
+  ${typography}
+  ${letterSpacing}
+`;
+
+export const TextAndImageBlock = ({
+  header = `Fresh Insights & a Century of Expertise`,
+  imgUrls,
+  subheader = `Education & Research`,
+  caption = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  btnText = `Button Text`,
+  btnUrl = `#`,
+}: TextAndImageBlockProps): JSX.Element => {
+  return (
+    <Box color="text" fontFamily="body" maxWidth={1440} m={'0 auto'}>
+      <Header
+        fontFamily="headline"
+        mx={[0]}
+        mt={0}
+        mb={[3, 3, 4]}
+        fontSize={[4, 3, 5]}
+        maxWidth={743}
+        fontWeight="regular"
+      >
+        {header}
+      </Header>
+      <Box
+        display="grid"
+        gridTemplateColumns={[
+          '1fr',
+          'minmax(368px,1fr) minmax(280px, 450px)',
+          'minmax(auto,743px) minmax(453px, 1fr)',
+        ]}
+        gridRowGap={0}
+        gridColumnGap={[0, 3, 6]}
+      >
+        <Box display={['none', 'none', 'block']} width="100%">
+          <Image src={imgUrls.desktop} width={743} height={419} />
+        </Box>
+        <Box gridColumn={1} display={['none', 'block', 'none']} width="100%">
+          <Image src={imgUrls.mobile} width={368} height={207.53} />
+        </Box>
+        <Box gridColumn={1} display={['block', 'none', 'none']} width="100%">
+          <Image src={imgUrls.mobile} width={280} height={304} />
+        </Box>
+        <Box
+          mt={[4, 0, 0]}
+          display="flex"
+          justifyContent="center"
+          flexDirection="column"
+        >
+          <Subheader m={0} fontFamily="Montserrat" fontSize={[0, '10px', 0]}>
+            {subheader}
+          </Subheader>
+          <Text
+            lineHeight={['body', 'body', 'heading']}
+            mt={[2, 1, 4]}
+            mb={[4, 2, 4]}
+            fontSize={[1, 0, 3]}
+            fontFamily="headline"
+            letterSpacing={3}
+          >
+            {caption}
+          </Text>
+          <Button
+            arrowColor="white"
+            arrow={true}
+            size="medium"
+            variant="solid"
+            url={btnUrl}
+            label={btnText}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
