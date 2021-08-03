fb2026c29fc51178f2250f863f36a153c941b5ce
adam tolley <adamt@hzdg.com>
2021-08-03 09:37:34 -0400

    comment->delete

--- web/components/IntroBlockComponent/_map.tsx
+++ web/components/IntroBlockComponent/_map.tsx
@@ -1,14 +1,6 @@
 import {IntroBlock, IntroBlockProps} from '.';
 import IntroBlockData from '@data/blocks/IntroBlockData';
 
-// // TODO: rearrange data into single var
-// const props = {
-//   description: `Nestled in the Berkshires, the Austen Riggs Center is unlike most mental health treatment centers. Here in our open setting in western Massachusetts, you’ll find the space and support you need to understand your problems, find your voice, and make your way forward.
-//   `,
-//   btnText: 'Learn About Us',
-//   btnUrl: '#',
-// };
-
 const _map = (block: IntroBlockData) => {
   const props: IntroBlockProps = {
     description: block.body,
