cabb5e556ccfeb2fdd24469ee0f898c8988d0e05
Adam Tolley <adamt@hzdg.com>
2021-09-13 12:44:57 -0400

    break out common page fields as partial in schema (#80)
    * fixes
    
    * extracted repeatable page query partial to use with other page types

--- sanity/schemas/documents/page.js
+++ sanity/schemas/documents/page.js
@@ -1,3 +1,5 @@
+import commonPageFields from '../partials/commonPageFields';
+
 export default {
   name: 'page',
   type: 'document',
@@ -9,32 +11,7 @@ export default {
     },
   ],
   fields: [
-    {
-      name: 'title',
-      type: 'string',
-      title: 'Title',
-    },
-    {
-      name: 'slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-      },
-    },
-    {
-      name: 'description',
-      type: 'text',
-      title: 'Description',
-      description: 'This description populates meta-tags on the webpage',
-      fieldset: 'metadata',
-    },
-    {
-      name: 'openGraphImage',
-      type: 'image',
-      title: 'Open Graph Image',
-      description: 'Image for sharing previews on Facebook, Twitter etc.',
-      fieldset: 'metadata',
-    },
+    ...commonPageFields,
     {
       name: 'blocks',
       type: 'array',
---
+++ sanity/schemas/partials/commonPageFields.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    name: 'title',
+    type: 'string',
+    title: 'Title',
+  },
+  {
+    name: 'slug',
+    type: 'slug',
+    options: {
+      source: 'title',
+    },
+  },
+  {
+    name: 'description',
+    type: 'text',
+    title: 'Description',
+    description: 'This description populates meta-tags on the webpage',
+    fieldset: 'metadata',
+  },
+  {
+    name: 'openGraphImage',
+    type: 'image',
+    title: 'Open Graph Image',
+    description: 'Image for sharing previews on Facebook, Twitter etc.',
+    fieldset: 'metadata',
+  },
+];
