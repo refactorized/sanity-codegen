2d76dc06c5817ae1ad1a082829be1350474dc7bf
adam tolley <adamt@hzdg.com>
2021-09-22 15:48:07 -0400

    edited link schema, add styled components to sanity project

--- sanity/schemas/objects/link.js
+++ sanity/schemas/objects/link.js
@@ -6,13 +6,6 @@ const link = {
     'Link to internal or external resources. Please enter information in only one field.',
   fields: [
     {
-      name: 'blockType',
-      type: 'string',
-      readOnly: true,
-      hidden: true,
-      initialValue: 'link',
-    },
-    {
       name: 'slug',
       title: 'Internal page slug',
       type: 'slug',
