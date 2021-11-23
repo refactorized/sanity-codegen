9c9a144d4c28b287113ce49f9940d44ccf3584ca
Wayne Denier <wayne.denier@gmail.com>
2021-11-23 10:06:14 -0500

    Arc 243 testimonial fix (#180)
    * Check photo for null, instead of empty string
    
    * disallow null and empty values

--- web/components/Card/index.tsx
+++ web/components/Card/index.tsx
@@ -282,7 +282,7 @@ export const TestimonialCard = ({
       </RightQuoteDiv>
       <TestimonialText>{testimonialText + '"'}</TestimonialText>
       <PatientLine>
-        {patientPhotoPath != null && (
+        {patientPhotoPath != null && patientPhotoPath != '' && (
           <PatientPhoto backgroundImage={patientPhotoPath}></PatientPhoto>
         )}
         <PatientName>- {patientName}</PatientName>
